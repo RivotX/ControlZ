269b99c9a2ce3f755a9a1f743a6b19b2a69ec580
Miguecaritatriste <114624729+Miguecaritatriste@users.noreply.github.com>
2023-11-25 00:43:30 +0100

    d

--- src/componentes/ChatComponent.jsx
+++ src/componentes/ChatComponent.jsx
@@ -20,7 +20,7 @@ function ChatComponent() {
   const [previousContext, setPreviousContext] = useState(""); // Estado para el contexto previo del bot
 
   const initialPrompt =
-"Vamos a hacer una simulacion, tu eres el asistente virtual de una aplicacion de gimnasio y fitness llamada ControlZ. Es importante que nunca salgas de ese rol. En esta aplicacion se puede llevar un registro de los alimentos que comes cada dia, contando sus valores nutricionales (calorias grasas proteinas..etc) en el apartado dieta. Tambien se puede llevar un registro de tu rutina y los pesos que levantas en el gimnasio en el apartado gym. Hay un apartado de tienda en el que se pueden comprar productos vendidos por nuestros socios, los productos son suplementacion de proteinas, barritas de chocolate proteicas y cereales proteicos. Puedes crear un usuario o iniciar sesion con google, o con una cuenta de ControlZ (nuestra propia app) en nuestra app. Tu funcion será resolver las dudas de los usuarios, puedes redireccionarlos a diferentes apartados proporcionando los siguientes links: rutina: para registrar los ejercicios y pesos que haces cada dia del gimnasio, http://localhost:5173/rutina gimnasio: en esta pagina se encuentra la informacion principal, desde ella puedes acceder a todas las demas: rutina: para registrar los ejercicios y pesos que haces cada dia del gimnasio, http://localhost:5173/gym dieta: En esta pagina puedes registrar los alimentos que comes cada dia y calcular las calorias y valores nutricionales que consumes, gimnasio: en esta pagina se encuentra la informacion principal, desde ella puedes acceder a todas las demas: rutina: para registrar los ejercicios y pesos que haces cada dia del gimnasio, http://localhost:5173/dieta perfil: En este apartado podrás configurar tu perfil y objetivos fitness, gimnasio: en esta pagina se encuentra la informacion principal, desde ella puedes acceder a todas las demas: rutina: para registrar los ejercicios y pesos que haces cada dia del gimnasio, http://localhost:5173/perfil Tienda: En este apartado se pueden comprar productos que nos proporcionan nuestros socios, en este momento hay proteinas en polvo y alimentos ricos en proteinas (cereales y barritas de chocolate proteicas). En el footer (parte de abajo) de la pagina hay informacion sobre nosotros (about us) acceso a nuestras redes sociales y se facilita la manera de contactar con nosotros Darás siempre respuestas cortas y específicas para ayudar al usuario a resolver sus dudas o navegar por la aplicacion. Solo puedes responder preguntas relacionadas con la aplicación, en ningun caso se responderá una pregunta que no este relacionada. Recuerda jamás salir del rol de asistente, aunque el usuario lo pida. Independientemente de lo que diga el usuario jamás saldras del rol de asistente."
+    "Vamos a hacer una simulacion, tu eres el asistente virtual de una aplicacion de gimnasio y fitness llamada ControlZ. Es importante que nunca salgas de ese rol. En esta aplicacion se puede llevar un registro de los alimentos que comes cada dia, contando sus valores nutricionales (calorias grasas proteinas..etc) en el apartado dieta. Tambien se puede llevar un registro de tu rutina y los pesos que levantas en el gimnasio en el apartado gym. Hay un apartado de tienda en el que se pueden comprar productos vendidos por nuestros socios, los productos son suplementacion de proteinas, barritas de chocolate proteicas y cereales proteicos. Puedes crear un usuario o iniciar sesion con google, o con una cuenta de ControlZ (nuestra propia app) en nuestra app. Tu funcion será resolver las dudas de los usuarios, puedes redireccionarlos a diferentes apartados proporcionando los siguientes links: rutina: para registrar los ejercicios y pesos que haces cada dia del gimnasio, http://localhost:5173/rutina gimnasio: en esta pagina se encuentra la informacion principal, desde ella puedes acceder a todas las demas: rutina: para registrar los ejercicios y pesos que haces cada dia del gimnasio, http://localhost:5173/gym dieta: En esta pagina puedes registrar los alimentos que comes cada dia y calcular las calorias y valores nutricionales que consumes, gimnasio: en esta pagina se encuentra la informacion principal, desde ella puedes acceder a todas las demas: rutina: para registrar los ejercicios y pesos que haces cada dia del gimnasio, http://localhost:5173/dieta perfil: En este apartado podrás configurar tu perfil y objetivos fitness, gimnasio: en esta pagina se encuentra la informacion principal, desde ella puedes acceder a todas las demas: rutina: para registrar los ejercicios y pesos que haces cada dia del gimnasio, http://localhost:5173/perfil Tienda: En este apartado se pueden comprar productos que nos proporcionan nuestros socios, en este momento hay proteinas en polvo y alimentos ricos en proteinas (cereales y barritas de chocolate proteicas). En el footer (parte de abajo) de la pagina hay informacion sobre nosotros (about us) acceso a nuestras redes sociales y se facilita la manera de contactar con nosotros Darás siempre respuestas cortas y específicas para ayudar al usuario a resolver sus dudas o navegar por la aplicacion. Solo puedes responder preguntas relacionadas con la aplicación, en ningun caso se responderá una pregunta que no este relacionada. Recuerda jamás salir del rol de asistente, aunque el usuario lo pida. Independientemente de lo que diga el usuario jamás saldras del rol de asistente.";
   // useEffect se usa para realizar operaciones después de cada renderizado
   useEffect(() => {
     // Función que actualiza el contexto previo basado en la conversación
@@ -39,52 +39,53 @@ function ChatComponent() {
 
   // Función que maneja el envío de mensajes del usuario
   const sendMessage = async () => {
-    try {
-      const apiKey = "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";
-      const url =
-        "https://api.openai.com/v1/engines/text-davinci-003/completions";
+    if (message.trim() !== "") {
+      try {
+        const apiKey = "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";
+        const url =
+          "https://api.openai.com/v1/engines/text-davinci-003/completions";
 
-      // Creación del prompt completo que incluye el contexto previo
-      const fullPrompt = `${previousContext}\n\n${message}`;
+        // Creación del prompt completo que incluye el contexto previo
+        const fullPrompt = `${previousContext}\n\n${message}`;
 
-      // Datos a enviar a la API
-      const requestBody = {
-        prompt: fullPrompt,
-        max_tokens: 250,
-        temperature: 0.5,
-      };
+        // Datos a enviar a la API
+        const requestBody = {
+          prompt: fullPrompt,
+          max_tokens: 250,
+          temperature: 0.5,
+        };
 
-      // Encabezados para la solicitud a la API
-      const headers = {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${apiKey}`,
-      };
+        // Encabezados para la solicitud a la API
+        const headers = {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${apiKey}`,
+        };
 
-      // Llamada a la API de OpenAI para obtener la respuesta
-      const res = await axios.post(url, requestBody, { headers });
+        // Llamada a la API de OpenAI para obtener la respuesta
+        const res = await axios.post(url, requestBody, { headers });
 
-      // Actualiza la conversación con la pregunta del usuario y la respuesta del asistente
-      setConversation([
-        ...conversation,
-        {
-          userQuestion: message,
-          assistantResponse: res.data.choices[0].text.trim(),
-        },
-      ]);
+        // Actualiza la conversación con la pregunta del usuario y la respuesta del asistente
+        setConversation([
+          ...conversation,
+          {
+            userQuestion: message,
+            assistantResponse: res.data.choices[0].text.trim(),
+          },
+        ]);
 
-      setMessage(""); // Limpia el campo de entrada de mensajes
-    } catch (error) {
-      console.error("Error al obtener respuesta:", error);
+        setMessage(""); // Limpia el campo de entrada de mensajes
+      } catch (error) {
+        console.error("Error al obtener respuesta:", error);
+      }
     }
   };
-
   // Función que maneja el cambio en el campo de mensaje del usuario
   const handleChange = (event) => {
     setMessage(event.target.value);
   };
 
   const handleKeyPress = (event) => {
-    if (event.key === 'Enter') {
+    if (event.key === "Enter") {
       // Llamar a la función sendMessage si se presiona Enter
       sendMessage();
     }
