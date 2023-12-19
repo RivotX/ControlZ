d0d5b6cf9bce0b3da3a435d54cd8cf80373e8d2b
RivotX <miguelrosso60@gmail.com>
2023-12-19 19:04:21 +0100

    sisisi

---
+++ Server/server.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import mysql from 'mysql';
+
+
+const app = express();
+app.use(express);
+
+const db= mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'usuarios',
+})
+
+app.listen(8081, () => {
+    console.log("funciona");
+})
+
