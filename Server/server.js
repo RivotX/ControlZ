63f9c2e626e3e8072a7e3a4b69707a991555bedc
Miguecaritatriste <114624729+Miguecaritatriste@users.noreply.github.com>
2023-12-25 22:59:26 +0100

    arreglos sintaxis jsx en index

--- Server/server.js
+++ Server/server.js
@@ -1,18 +1,16 @@
-import express from 'express';
-import mysql from 'mysql';
-
+import express from "express";
+import mysql from "mysql";
 
 const app = express();
 app.use(express);
 
-const db= mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'usuarios',
-})
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "usuarios",
+});
 
-app.listen(5175, () => {
-    console.log("servidor corriendo...");
-})
-
+app.listen(8081, () => {
+  console.log("servidor corriendo...");
+});
