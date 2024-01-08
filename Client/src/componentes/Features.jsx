c15ebb7f85e80bf4d1c6b861ea3eb0dce8fde833
DaniCaba09 <dcaba.sanchez@gmail.com>
2024-01-08 21:01:20 +0100

    ttt

--- Client/src/componentes/Features.jsx
+++ Client/src/componentes/Features.jsx
@@ -2,14 +2,14 @@ export default Features;
 
 function Features({ imagen, titulo, descripcion, link }) {
   return (
-    <a href={link} class="text-decoration-none">
-      <div class="col ">
-        <div class="card m-2 hovcard">
-          <img src={imagen} class="card-img-top " alt="..." />
-          <div class="card-body bg-dark ">
-            <h5 class="card-title text-white">{titulo}</h5>
-            <p class="card-text text-secondary cartas">{descripcion}</p>
-            <span href={link} class="btn btn-outline-info">
+    <a href={link} className="text-decoration-none">
+      <div className="col ">
+        <div className="card m-2 hovcard">
+          <img src={imagen} className="card-img-top " alt="..." />
+          <div className="card-body bg-dark ">
+            <h5 className="card-title text-white">{titulo}</h5>
+            <p className="card-text text-secondary cartas">{descripcion}</p>
+            <span href={link} className="btn btn-outline-info">
               Acceder
             </span>
           </div>
