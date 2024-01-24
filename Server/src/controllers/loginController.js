161bff27d83e462fa5fb3b26cdfc231182d04d91
RivotX <miguelrosso60@gmail.com>
2024-01-24 20:02:24 +0100

    reestructurado

---
+++ Server/src/controllers/loginController.js
@@ -0,0 +1,47 @@
+// controllers/authController.js
+import bcrypt from "bcrypt";
+import { db } from "../config/db.js";
+
+const login = async (req, res) => {
+  const consulta = "SELECT * FROM usuarios WHERE usuario = ?";
+  const values = [req.body.usuario];
+
+  try {
+    const result = await new Promise((resolve, reject) => {
+      db.query(consulta, values, (err, result) => {
+        if (err) {
+          console.log("Error en la consulta:", err);
+          reject("Error en la consulta a la base de datos");
+        } else {
+          resolve(result);
+        }
+      });
+    });
+
+    if (result.length > 0) {
+      const passwordFromDB = result[0].password;
+      const IsCorrect = await bcrypt.compare(req.body.password, passwordFromDB);
+
+      if (IsCorrect) {
+        req.session.usuario = result[0].usuario;
+        req.session.nombre = result[0].nombre;
+        req.session.email = result[0].email;
+        req.session.telefono = result[0].telefono;
+        req.session.direccion = result[0].direccion;
+        req.session.sexo = result[0].sexo;
+
+        console.log(req.session);
+        return res.send({ status: "correcto", redirectTo: "/principal" });
+      } else {
+        return res.json({ Error: "Contraseña incorrecta" });
+      }
+    } else {
+      return res.status(201).json({ Error: "No existe el usuario" });
+    }
+  } catch (error) {
+    console.error("Error general:", error);
+    return res.status(500).json({ Error: "Error interno del servidor" });
+  }
+};
+
+export { login };
--- Server/src/server.js
+++ Server/src/server.js
@@ -1,17 +1,17 @@
 // set up
 import express from "express";
 import mysql from "mysql";
-import cors from "cors"; 
-import bcrypt from "bcrypt";
+import cors from "cors";
 import Mongoose from "mongoose";
 import session from "express-session";
+import { registro } from "./controllers/registroController.js";
+import { existeRegistro } from "./controllers/registroController.js";
 import { getRutina } from "./controllers/rutinaController.js";
 import { db } from "./config/db.js";
 import { CreaRutina } from "./models/rutinaModel.js";
-
+import { login } from "./controllers/authController.js";
 
 //hash para contraseña
-const salt = 10;
 
 const app = express();
 
@@ -48,36 +48,12 @@ Mongoose.connect("mongodb://127.0.0.1:27017/rutina")
     console.error("Error de conexión a MongoDB:", error);
   });
 
-const mongodb = Mongoose.connection;
-
-const diasSchema = new Mongoose.Schema({
-  ejercicio: {
-    id: Number,
-    series: Number,
-    repeticiones: Number,
-  },
-});
-
-const rutinaSchema = new Mongoose.Schema({
-  id: String,
-  lunes: [diasSchema],
-  martes: [diasSchema],
-  miercoles: [diasSchema],
-  jueves: [diasSchema],
-  viernes: [diasSchema],
-  sabado: [diasSchema],
-  domingo: [diasSchema],
-});
-
 // const CreaRutina = Mongoose.model("rutina", rutinaSchema);
-
-
 
 app.listen(8081, () => {
   console.log("servidor corriendo...");
 });
 
-//Endpoint /logout
 
 app.get("/logout", (req, res) => {
   // Destruir la sesión actual
@@ -91,183 +67,21 @@ app.get("/logout", (req, res) => {
   });
 });
 
-//Endpoint /getSession
-
 app.get("/getSession", (req, res) => {
   console.log(req.session);
 
   res.json(req.session);
 });
 
-// //Endpoint /getrutina
-// app.post("/getrutina", async (req, res) => {
-//   const user = req.body.usuario;
-
-//   try {
-//     const Schema = new Mongoose.Schema({}, { strict: false });
-
-//     const datos = await CreaRutina.find({ id: user });
-//     console.log(datos);
-//     res.json(datos);
-//   } catch (error) {
-//     console.log(error);
-//     res.status(500).json({ mensaje: "Error al obtener datos" });
-//   }
-// });
-
 app.use("/getrutina", getRutina);
 
-
-//End /saveRutina
-app.post("/saveRutina",async (req,res)=>{
-
-  const {user, lunes, martes, miercoles, jueves, viernes, sabado, domingo } = req.body;
-
-
+app.post("/saveRutina", async (req, res) => {
+  const { user, lunes, martes, miercoles, jueves, viernes, sabado, domingo } =
+    req.body;
 });
 
-// Endpoint /registro
+app.post("/registro", registro);
 
-app.post("/registro", (req, res) => {
-  const consulta =
-    "INSERT INTO usuarios (`usuario`, `password`,`nombre`,`email` ,`telefono` ,`direccion` ) VALUES (?,?,?,?,?,?)";
-  //hasheo de la contraseña, (error,hash) es el ultimo parametro que recibe la funcion de bcrypt, llamado funcion de callback.
-  bcrypt.hash(req.body.password.toString(), salt, (error, hash) => {
-    if (error) {
-      return res.json({ Error: "error for hashing password" });
-    } else {
-      const values = [
-        req.body.usuario,
-        hash,
-        req.body.nombre,
-        req.body.email,
-        req.body.telefono,
-        req.body.direccion,
-        // req.body.sexo
-      ];
-      const rutina = new CreaRutina({
-        id: req.body.usuario,
+app.post("/existeregistro", existeRegistro);
 
-        lunes: [],
-        martes: [],
-        miercoles: [],
-        jueves: [],
-        viernes: [],
-        sabado: [],
-        domingo: [],
-      });
-
-      rutina
-        .save()
-        .then((resultado) => {
-          console.log(resultado);
-        })
-        .catch((err) => {
-          console.error(err);
-        });
-
-      db.query(consulta, values, (err, result) => {
-        if (err) {
-          console.error("Error al insertar en la base de datos:", err);
-          return res
-            .status(500)
-            .json({ Error: "Error al insertar en la base de datos =====" });
-        } else {
-          return res.json({ Status: "success" });
-        }
-      });
-    }
-  });
-});
-
-//Endpoint / ExisteRegistro
-app.post("/existeregistro", async (req, res) => {
-  const consultaUsuario = "SELECT * FROM USUARIOS WHERE USUARIO=?";
-  const consultaEmail = "SELECT * FROM USUARIOS WHERE EMAIL=?";
-
-  const usuario = req.body.usuario;
-  const email = req.body.email;
-
-  let existeUsuario = false;
-  let existeEmail = false;
-
-  function consultarDB(consulta, parametro) {
-    return new Promise((resolve, reject) => {
-      db.query(consulta, parametro, (err, result, campos) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(result);
-        }
-      });
-    });
-  }
-
-  try {
-    const usuarioResult = await consultarDB(consultaUsuario, usuario);
-    if (usuarioResult.length > 0) {
-      existeUsuario = true;
-    }
-
-    const emailResult = await consultarDB(consultaEmail, email);
-    if (emailResult.length > 0) {
-      existeEmail = true;
-    }
-
-    if (existeUsuario) {
-      return res.status(201).json({ Status: "Existe el usuario" });
-    } else if (existeEmail) {
-      return res.status(201).json({ Status: "Existe el email" });
-    } else {
-      return res.json({ Status: "Success" });
-    }
-  } catch (error) {
-    console.error("Error en la base de datos:", error);
-    return res
-      .status(500)
-      .json({ Error: "Error al comprobar existencia del registro" });
-  }
-});
-
-//SESSION Y LOGIN MEJORADO --las sessiones inician al logearse¡¡¡¡
-app.post("/login", async (req, res) => {
-  const consulta = "SELECT * FROM usuarios WHERE usuario = ?";
-  const values = [req.body.usuario];
-
-  try {
-    const result = await new Promise((resolve, reject) => {
-      db.query(consulta, values, (err, result) => {
-        if (err) {
-          console.log("Error en la consulta:", err);
-          reject("Error en la consulta a la base de datos");
-        } else {
-          resolve(result);
-        }
-      });
-    });
-
-    if (result.length > 0) {
-      const passwordFromDB = result[0].password;
-      const IsCorrect = await bcrypt.compare(req.body.password, passwordFromDB);
-
-      if (IsCorrect) {
-        req.session.usuario = result[0].usuario;
-        req.session.nombre = result[0].nombre;
-        req.session.email = result[0].email;
-        req.session.telefono = result[0].telefono;
-        req.session.direccion = result[0].direccion;
-        req.session.sexo = result[0].sexo;
-
-        console.log(req.session);
-        return res.send({ status: "correcto", redirectTo: "/principal" });
-      } else {
-        return res.json({ Error: "Contraseña incorrecta" });
-      }
-    } else {
-      return res.status(201).json({ Error: "No existe el usuario" });
-    }
-  } catch (error) {
-    console.error("Error general:", error);
-    return res.status(500).json({ Error: "Error interno del servidor" });
-  }
-});
+app.post("/login", login);
