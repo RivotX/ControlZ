b90a2878393cda12e93eb5a356db633626d95d0c
RivotX <miguelrosso60@gmail.com>
2024-02-06 17:26:53 +0100

    commet

---
+++ Client/src/components/GraficaDieta.jsx
@@ -0,0 +1,45 @@
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+import '../styles/custom.css';
+
+const Grafica = () => {
+    let percentage = 70;
+    return (
+        <div className="tw-relative tw-w-32 tw-h-32"> {/* Contenedor para la gráfica */}
+            <CircularProgressbar
+                value={percentage}
+                text={""} // No necesitamos el texto aquí
+                className="custom-text" // Aplica una clase CSS para el contenedor del texto
+
+                styles={buildStyles({
+                    // Rotation of path and trail, in number of turns (0-1)
+                    rotation: 0.25,
+
+                    // Whether to use rounded or flat corners on the ends - can use 'butt' or 'round'
+                    strokeLinecap: 'butt',
+
+                    // Text size
+                    textSize: '10px',
+
+                    // How long animation takes to go from one percentage to another, in seconds
+                    pathTransitionDuration: 0.5,
+
+                    // Can specify path transition in more detail, or remove it entirely
+                    // pathTransition: 'none',
+
+                    // Colors
+                    pathColor: `rgba(62, 152, 199, ${percentage / 100})`,
+                    textColor: 'white',
+                    trailColor: '#d6d6d6',
+                    backgroundColor: '#3e98c7',
+                })}
+            />
+            <div className="tw-absolute tw-inset-0 tw-flex tw-flex-col tw-items-center tw-justify-center text-container">
+                <div className="text-number tw-font-bold">579</div>
+                <div className="text-calories tw-text-sm">Calorías</div>
+            </div>
+        </div>
+    );
+};
+
+export default Grafica;
--- Client/src/pages/Dieta.jsx
+++ Client/src/pages/Dieta.jsx
@@ -4,8 +4,9 @@ import add from '../img/add (1).png'
 import FoodModal from "../components/FoodModal";
 import { useState } from "react";
 import cafe from "../img/Deus_Coffee.png"
-import image from "../img/image.png"
 import Footer from "../components/Footer";
+import Grafica from "../components/GraficaDieta";
+
 
 function Dieta() {
   const [ShowFoodModal, SetShowFoodModal] = useState(false);
@@ -22,8 +23,33 @@ function Dieta() {
       <div className="tw-min-h-screen  tw-bg-[#0d0d0d] tw-pt-[4.87rem] tw-px-4" style={{ fontFamily: 'system-ui, -apple-system, BlinkMacSystemFont' }}>
         <Navbar linkHome={"/gym"} />
         <div className="tw-flex tw-justify-center tw-w-full tw-flex-wrap tw-text-white tw-rounded-md">
-          <p className="tw-w-full tw-bg-[#292929] tw-mt-3 tw-text-center tw-p-3">(anterior)---- HOY ---(siguente)</p>
-          <img src={image}></img>
+          <div className="tw-w-full tw-bg-[#292929] tw-mt-3 tw-text-center tw-p-3">
+            <div className="tw-flex tw-justify-between tw-px-5">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="tw-w-[30px] tw-h-[30px]">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
+              </svg>
+
+              <span className="tw-w-full tw-font-bold"> HOY </span>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="tw-w-[30px] tw-h-[30px]">
+                <path strokeLinecap="round" strokeLinejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
+              </svg>
+            </div>
+
+
+          </div>
+          <div className="tw-flex tw-items-center tw-justify-between tw-mt-5 tw-pointer-events-none tw-w-full tw-text-sm tw-px-3">
+          <div className="tw-flex tw-flex-wrap tw-justify-center tw-w-1/5" >
+              <p className="tw-w-full tw-flex tw-justify-center tw-font-bold"><span>2421</span></p>
+              <p className="tw-w-full tw-flex tw-justify-center"><span>Consumidas</span></p>
+            </div>
+            <div className="tw-w-2/5">
+              <Grafica />
+            </div>
+            <div className="tw-flex tw-flex-wrap tw-justify-center tw-w-1/5" >
+              <p className="tw-w-full tw-flex tw-justify-center tw-font-bold"><span>3000</span></p>
+              <p className="tw-w-full tw-flex tw-justify-center"><span>Objetivo</span></p>
+            </div>
+          </div>
           <div className="tw-w-full tw-min-h-[60vh] tw-bg-[#292929] tw-roudned-md tw-flex tw-flex-wrap tw-mb-[5vh] tw-justify-between tw-p-3 tw-rounded-md tw-mt-[5vh] tw-px-5">
 
             <div className="tw-border-gray-400 tw-w-full tw-flex tw-flex-wrap tw-h-1/4 tw-border-b " >
