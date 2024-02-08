48cc6b6409f9f0cc1b6f34e93871289da9e6be4b
ChanGuaZzz <gga0006@alu.medac.es>
2024-02-08 15:26:15 +0100

    gg

--- Client/src/components/ModalEjercicio.jsx
+++ Client/src/components/ModalEjercicio.jsx
@@ -2,7 +2,9 @@ export default function ModalEjercicio({ modalvisible }) {
   return (
     <>{modalvisible&&
     <div className=" NegroOpacidad75 tw-fixed tw-left-0 tw-top-0 tw-z-[51] tw-flex tw-h-screen tw-w-screen tw-items-center tw-justify-center">
-      <div className="b tw-bg-slate-700 tw-h-[70%] tw-w-[70%]">dwwd</div>
+      <div className="tw-rounded-3xl tw-bg-slate-700 tw-h-[50%] tw-w-[50%] tw-py-4 tw-px-10">
+        <div className="  tw-border-b tw-pb-4 tw-border-slate-400 tw-font-semibold tw-text-lg ">Añadir ejercicio</div>
+      </div>
     </div>
 
     }</>
