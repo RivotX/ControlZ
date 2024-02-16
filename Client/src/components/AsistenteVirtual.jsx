082c47d73d8f2a97b2a180752ccf105d39ced4e8
Miguecaritatriste <114624729+Miguecaritatriste@users.noreply.github.com>
2024-02-16 22:35:15 +0100

    limpiando un poquito el codigo

--- Client/src/components/AsistenteVirtual.jsx
+++ Client/src/components/AsistenteVirtual.jsx
@@ -268,8 +268,8 @@ function AsistenteVirtual() {
                   <path strokeLinecap="round" strokeLinejoin="round" d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5" />
                 </svg>
               </div>
-            </div>)
-
+            </div>
+            )
         )
       }
     </>
